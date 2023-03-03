3a79cee2382879bfe7b04a8d670efbb13b57963b
sophia656 <antropovasophia@gmail.com>
2023-03-03 17:26:23 +0300

    Updates

---
+++ static/js/266.751e980c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[266],{266:function(n,e,t){t.r(e),t.d(e,{default:function(){return u}});t(791);var i,r=t(168),c=t(444).ZP.div(i||(i=(0,r.Z)(["\n    width: 100vw;\n    height: 100vh;\n    // background: #9D2763;\n    color: white;\n    display: flex;\n    align-items: center;\n    justify-content: center;\n"]))),o=t(184),u=function(){return(0,o.jsx)(c,{children:"faces"})}}}]);
+//# sourceMappingURL=266.751e980c.chunk.js.map
