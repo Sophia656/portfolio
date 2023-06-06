b2f4704ea8ca81c8f8d3b16a802b81de69ab07fd
sophia656 <antropovasophia@gmail.com>
2023-06-06 16:23:33 +0300

    desc

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -22,12 +22,6 @@ const Contacts = () => {
                 <Avatar onContextMenu={onHandleRightClick} src={avatar} alt="" />
                 <Description>
                     I am commited to a humanistic understanding of life that is centered on the essence of the human being. Where from and content are inextricably linked and the human body is a perfection of his inner world. In other words, the shot captured in an instant is the quintessence of the conscious and the unconscious.
-                    <br/>
-                    --------------------------------------------------
-                    <br/>
-                    {/* В фотографии нет объективного "до" и "после", есть только момент — 1/200 секунды. Она не расскажет вам, что было за кадром, сколько сотен практически одинаковых снимков было сделано, в какой обстановке она была снята. В ней лишь мгновение, видение случайно явившегося мира.
-                    <br/> */}
-                    Я опираюсь на гуманистическое познание жизни, в центре которого находится сущность человека. Где форма и содержание неразрывно связаны, а человеческое тело является отражением его внутреннего мира. Иными словами, запечатленный во мгновении снимок является квинтэссенцией осознанного и бессознательного.
                 </Description>
             </DescWrap>
             <Footer />
